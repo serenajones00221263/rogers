f4034eb8aaf84763f068306a2881593451825033
serenajones00221263 <141944552+serenajones00221263@users.noreply.github.com>
2023-08-10 09:34:17 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_9d8u3ts", "template_ttcdfqn", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
